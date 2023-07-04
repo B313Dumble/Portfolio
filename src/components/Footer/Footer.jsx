5a2801a6b886d39de42ae5a6d9722b257620b0e3
Giorgi Maisuradze <giorgimaisuradze762@gmail.com>
2023-07-04 18:32:46 +0400

    last

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,27 +5,27 @@ const Footer = () => {
   const sites = [
     {
       name: "Movies",
-      url: "https://site1.com",
+      url: "https://venerable-biscochitos-c79011.netlify.app/",
       image: "img/portfolio-05.jpg",
     },
     {
       name: "Shoping",
-      url: "https://site2.com",
+      url: "https://maisuradze-crwn-clothing.netlify.app/",
       image: "img/portfolio-04.jpg",
     },
     {
       name: "Meme Generation",
-      url: "https://site3.com",
+      url: "https://animated-tulumba-1186ff.netlify.app/",
       image: "img/portfolio-06.jpg",
     },
     {
       name: "Notes",
-      url: "https://site4.com",
+      url: "https://astounding-moonbeam-c27570.netlify.app/",
       image: "img/portfolio-08.jpg",
     },
     {
       name: "Modes",
-      url: "https://site5.com",
+      url: "https://heroic-longma-a8a30e.netlify.app/",
       image: "img/portfolio-02.jpg",
     },
   ];
